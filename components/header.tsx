30850b00467341020418c59dbd209da3dd80c358
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-08-23 20:00:00 +0900

    chore: 軽い修正

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 z-10 h-[65px] flex items-center justify-between bg-white border-b">
       <div className="ml-[32px] flex gap-[16px]">
         <Image src="/icon.svg" alt="Progate Logo" width={16} height={16} />
-        <p className="font-Plus Jakarta Sans">夏のビンゴ</p>
+        <p className="font-Plus Jakarta Sans">夏のビンゴ </p>
       </div>
       <Button className="mr-[32px]" variant="ghost" size="icon">
         <Avatar>
