4922237b9e8d025bfa62b7011689c252dcc6b84f
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-08-23 13:59:13 +0900

    refactor: 細かい修正

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,25 +9,19 @@ export default function LoginPage() {
         <h1 className="text-[28px] text-black font-bold">
           Log In to your account
         </h1>
-        <div className="">
-          <Input
-            type="username"
-            placeholder="Username"
-            className="h-[56px] bg-[#E8EDF5]"
-          />
-        </div>
-        <div className="">
-          <Input
-            type="password"
-            placeholder="Password"
-            className="h-[56px] bg-[#E8EDF5]"
-          />
-        </div>
-        <div className="">
-          <Button className="w-[480px] h-[48px] rounded-md bg-[#0D80F2] text-white">
-            Log In
-          </Button>
-        </div>
+        <Input
+          type="username"
+          placeholder="Username"
+          className="h-[56px] bg-[#E8EDF5]"
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          className="h-[56px] bg-[#E8EDF5]"
+        />
+        <Button className="w-[480px] h-[48px] rounded-md bg-[#0D80F2] text-white">
+          Log In
+        </Button>
       </div>
     </div>
   );
--- app/(auth)/setup/page.tsx
+++ app/(auth)/setup/page.tsx
@@ -9,38 +9,27 @@ export default function SetupPage() {
         <h1 className="text-[28px] text-black font-bold">
           Create your account
         </h1>
-        <div className="">
-          <Input
-            type="username"
-            placeholder="Username"
-            className="h-[56px] bg-[#E8EDF5]"
-          />
-        </div>
-        <div className="">
-          <Input
-            type="email"
-            placeholder="Email"
-            className="h-[56px] bg-[#E8EDF5]"
-          />
-        </div>
-
-        <div className="">
-          <Input
-            type="password"
-            placeholder="Password"
-            className="h-[56px] bg-[#E8EDF5]"
-          />
-        </div>
-        <div className="">
-          <Button className="w-[480px] h-[48px] rounded-md bg-[#0D80F2] text-[#F7FAFC]">
-            Create Account
-          </Button>
-        </div>
-        <div className="">
-          <Button className="w-[480px] h-[48px] rounded-md bg-[#E8EDF5] text-[#0D141C]">
-            Log In
-          </Button>
-        </div>
+        <Input
+          type="username"
+          placeholder="Username"
+          className="h-[56px] bg-[#E8EDF5]"
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          className="h-[56px] bg-[#E8EDF5]"
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          className="h-[56px] bg-[#E8EDF5]"
+        />
+        <Button className="w-[480px] h-[48px] rounded-md bg-[#0D80F2] text-[#F7FAFC]">
+          Create Account
+        </Button>
+        <Button className="w-[480px] h-[48px] rounded-md bg-[#E8EDF5] text-[#0D141C]">
+          Log In
+        </Button>
       </div>
     </div>
   );
