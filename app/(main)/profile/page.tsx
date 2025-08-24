a1bddc1d0af40888ae615ea36278f209384202db
masato626 <m.takiguchi.22.7.561@4nonome.com>
2025-08-24 19:03:37 +0900

    階層を変更

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Avatar, AvatarImage } from "../../components/ui/avatar";
-import { Button } from "../../components/ui/button";
+import { Avatar, AvatarImage } from "../../../components/ui/avatar";
+import { Button } from "../../../components/ui/button";
 import {
   Table,
   TableBody,
