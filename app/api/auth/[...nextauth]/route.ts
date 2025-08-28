e482e7ddffd1dc2a26ed1447cc6404220ec8194a
yuu-1230 <yuto.nagata1230@gmail.com>
2025-08-29 08:21:01 +0900

    authの設定修正

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,14 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { query } from "@/lib/db";
 import bcrypt from "bcrypt";
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
   providers: [
     CredentialsProvider({
-      name: "Credentials",
       credentials: {
         username: { label: "Username", type: "text" },
         password: { label: "Password", type: "password" },
@@ -18,20 +17,16 @@ const handler = NextAuth({
         if (!credentials?.username || !credentials.password) {
           return null;
         }
-
         const { rows } = await query(
           'SELECT * FROM "user" WHERE username = $1',
           [credentials.username]
         );
         const user = rows[0];
-
         if (!user) return null;
-
         const isPasswordCorrect = await bcrypt.compare(
           credentials.password,
           user.password
         );
-
         if (isPasswordCorrect) {
           return {
             id: user.id.toString(),
@@ -39,15 +34,30 @@ const handler = NextAuth({
             email: user.email,
           };
         }
-
         return null;
       },
     }),
   ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (session.user) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
   pages: {
     signIn: "/login",
   },
   secret: process.env.AUTH_SECRET,
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
