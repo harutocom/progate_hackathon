4f2ae42dda9f5ee1a07d9ddb1e7785c1c97eb8f5
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-08-28 16:49:28 +0900

    feat: 新規登録APIの繋ぎこみ

--- app/(auth)/setup/page.tsx
+++ app/(auth)/setup/page.tsx
@@ -1,35 +1,76 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function SetupPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await fetch("/api/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, email, password }),
+    });
+
+    if (response.ok) {
+      alert("登録が成功しました！ログインページに移動します。");
+      router.push("/login");
+      console.log("登録成功！");
+    } else {
+      alert("登録に失敗しました。もう一度お試しください。");
+      console.error("登録失敗...");
+    }
+  };
+
   return (
     <div className="mt-[52px] min-h-screen bg-white">
       <div className="mt-[200px] flex flex-col gap-8 items-center">
         <h1 className="text-[28px] text-black font-bold">
           Create your account
         </h1>
-        <Input
-          type="username"
-          placeholder="Username"
-          className="h-[56px] bg-[#E8EDF5]"
-        />
-        <Input
-          type="email"
-          placeholder="Email"
-          className="h-[56px] bg-[#E8EDF5]"
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          className="h-[56px] bg-[#E8EDF5]"
-        />
-        <Button className="w-[480px] h-[48px] rounded-md bg-[#0D80F2] text-[#F7FAFC]">
-          Create Account
-        </Button>
-        <Button className="w-[480px] h-[48px] rounded-md bg-[#E8EDF5] text-[#0D141C]">
-          Log In
-        </Button>
+        <form
+          action=""
+          className="flex flex-col gap-8 items-center"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            type="text"
+            placeholder="Username"
+            className="h-[56px] bg-[#E8EDF5]"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            className="h-[56px] bg-[#E8EDF5]"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            className="h-[56px] bg-[#E8EDF5]"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button className="w-[480px] h-[48px] rounded-md bg-[#0D80F2] text-[#F7FAFC]">
+            Create Account
+          </Button>
+          <Button
+            asChild
+            className="w-[480px] h-[48px] rounded-md bg-[#E8EDF5] text-[#0D141C]"
+          >
+            <Link href="/login">Log In</Link>
+          </Button>
+        </form>
       </div>
     </div>
   );
